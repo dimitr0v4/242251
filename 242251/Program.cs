42ed6dc553a1ca69bde45e531d1293f9a52926dd
MEGA <MEGA@gmail.com>
2025-02-24 17:23:31 +0200

    Add project files.

---
+++ 242251/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _242251
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] test=new int[]{1,3,4,5,4,6,4,7,5,1,4,3 };
+            Console.WriteLine(Search(test,4));
+            
+        }
+        public static int Search(int[] array,int target)
+        { int count=0;
+            for (int i=0;i<array.Length;i++)
+            { if (array[i]==target)
+                {count++; }
+            
+            }
+            if( count>0){return count;}
+            else{return 0; } }
+    }
+}
---
+++ 242252/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _242252
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
